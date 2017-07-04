c10d01999a793001b53cf445adb7350ddb6e57dc
David Alencar <davidalencarignacio@gmail.com>
2017-07-04 13:12:14 -0300

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,7 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createAt: new Date().getTime()
+  });
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Wellcome!!!',
+    createAt: new Date().getTime()
+  });
+
+
   socket.on('createMessage', (msg) => {
     console.log('createMessage', msg);
     io.emit('newMessage', {
@@ -23,6 +35,13 @@ io.on('connection', (socket) => {
       text: msg.text,
       createAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: msg.from,
+    //   text: msg.text,
+    //   createAt: new Date().getTime()
+    // });
+
   })
 
   socket.on('disconnect', () => {
